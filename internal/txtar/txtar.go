84e093dc5b59bc2c476e75df1a9ec4529cd2d159
Matthew Mueller <mattmuelle@gmail.com>
2021-11-18 21:45:27 -0600

    add txtar

---
+++ internal/txtar/txtar.go
@@ -0,0 +1,19 @@
+package txtar
+
+import (
+	"gitlab.com/mnm/bud/vfs"
+	"golang.org/x/tools/txtar"
+)
+
+// ParseFile parse a txtar file into a virtual filesystem. Used for tests
+func ParseFile(path string) (vfs.Memory, error) {
+	archive, err := txtar.ParseFile(path)
+	if err != nil {
+		return nil, err
+	}
+	memory := vfs.Memory{}
+	for _, file := range archive.Files {
+		memory[file.Name] = &vfs.File{Data: file.Data}
+	}
+	return memory, nil
+}
---
+++ internal/txtar/txtar_test.go
@@ -0,0 +1,30 @@
+package txtar_test
+
+import (
+	"io/fs"
+	"testing"
+
+	"github.com/matryer/is"
+	"gitlab.com/mnm/bud/internal/txtar"
+)
+
+func TestParseFile(t *testing.T) {
+	is := is.New(t)
+	fsys, err := txtar.ParseFile("testdata/one.txt")
+	is.NoErr(err)
+	des, err := fs.ReadDir(fsys, ".")
+	is.NoErr(err)
+	is.Equal(len(des), 3)
+	is.Equal(des[0].Name(), "a.go")
+	is.Equal(des[1].Name(), "b")
+	is.Equal(des[2].Name(), "c")
+	code, err := fs.ReadFile(fsys, "a.go")
+	is.NoErr(err)
+	is.Equal(string(code), "package a\n\n")
+	code, err = fs.ReadFile(fsys, "b/b.go")
+	is.NoErr(err)
+	is.Equal(string(code), "package b\n\n")
+	code, err = fs.ReadFile(fsys, "c/c/c.txt")
+	is.NoErr(err)
+	is.Equal(string(code), "c\n")
+}
