4a2e7f30cd759bb44e73ffb9116a183d45a4a643
Matthew Mueller <mattmuelle@gmail.com>
2022-04-16 18:35:46 -0500

    hacky fix for re-hydating breaking the event listeners

--- livebud/runtime/svelte/index.ts
+++ livebud/runtime/svelte/index.ts
@@ -4,7 +4,15 @@ import { HydrateInput } from ".."
 // - Support frames
 // - Handle errors
 export default function createView(input: HydrateInput) {
-  const component = new input.page({
+  if (input.target != null) {
+    // TODO: for some reason Svelte isn't able to re-hydrate over itself during
+    // a live reload. I wonder if they've figured this out in SvelteKit, but you
+    // end up with a runtime error: "Cannot read properties of null (reading
+    // 'removeChild')". Encountering this error will depend on your Svelte code.
+    // For now, we'll clear the DOM in our target before hydrating.
+    input.target.innerHTML = ""
+  }
+  new input.page({
     target: input.target,
     props: input.props,
     hydrate: true,
