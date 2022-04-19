46a31885014e6be9de0793f06bcbcd7ddf8ebe7b
Matthew Mueller <mattmuelle@gmail.com>
2022-04-19 01:46:36 -0500

    improve watcher so it doesn't timeout as often and also so it watches and triggers created files within a created directory

--- package/watcher/watcher_test.go
+++ package/watcher/watcher_test.go
@@ -2,6 +2,7 @@ package watcher_test
 
 import (
 	"context"
+	"errors"
 	"os"
 	"path/filepath"
 	"testing"
@@ -13,6 +14,32 @@ import (
 	"github.com/livebud/bud/package/vfs"
 	"github.com/matryer/is"
 )
+
+var waitForEvents = 500 * time.Millisecond
+
+func writeFiles(dir string, files map[string]string) error {
+	eg := new(errgroup.Group)
+	for path, data := range files {
+		path := filepath.Join(dir, path)
+		data := data
+		eg.Go(func() error {
+			if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+				return err
+			}
+			return os.WriteFile(path, []byte(data), 0644)
+		})
+	}
+	return eg.Wait()
+}
+
+func getEvent(event <-chan string) (string, error) {
+	select {
+	case path := <-event:
+		return path, nil
+	case <-time.After(1 * time.Second):
+		return "", errors.New("timed out while waiting for watcher")
+	}
+}
 
 func TestChange(t *testing.T) {
 	is := is.New(t)
@@ -34,15 +61,12 @@ func TestChange(t *testing.T) {
 			return nil
 		})
 	})
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(waitForEvents)
 	err = os.WriteFile(filepath.Join(dir, "a.txt"), []byte("b"), 0644)
 	is.NoErr(err)
-	select {
-	case path := <-event:
-		is.Equal(path, filepath.Join(dir, "a.txt"))
-	case <-time.After(5 * time.Second):
-		t.Fatal("timed out while waiting for watcher")
-	}
+	path, err := getEvent(event)
+	is.NoErr(err)
+	is.Equal(path, filepath.Join(dir, "a.txt"))
 	cancel()
 	is.NoErr(eg.Wait())
 }
@@ -67,15 +91,12 @@ func TestDelete(t *testing.T) {
 			return nil
 		})
 	})
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(waitForEvents)
 	err = os.RemoveAll(filepath.Join(dir, "a.txt"))
 	is.NoErr(err)
-	select {
-	case path := <-event:
-		is.Equal(path, filepath.Join(dir, "a.txt"))
-	case <-time.After(5 * time.Second):
-		t.Fatal("timed out while waiting for watcher after 5s")
-	}
+	path, err := getEvent(event)
+	is.NoErr(err)
+	is.Equal(path, filepath.Join(dir, "a.txt"))
 	cancel()
 	is.NoErr(eg.Wait())
 }
@@ -96,15 +117,12 @@ func TestCreate(t *testing.T) {
 			return nil
 		})
 	})
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(waitForEvents)
 	err := os.WriteFile(filepath.Join(dir, "a.txt"), []byte("b"), 0644)
 	is.NoErr(err)
-	select {
-	case path := <-event:
-		is.Equal(path, filepath.Join(dir, "a.txt"))
-	case <-time.After(5 * time.Second):
-		t.Fatal("timed out while waiting for watcher")
-	}
+	path, err := getEvent(event)
+	is.NoErr(err)
+	is.Equal(path, filepath.Join(dir, "a.txt"))
 	cancel()
 	is.NoErr(eg.Wait())
 }
@@ -125,22 +143,62 @@ func TestCreateRecursive(t *testing.T) {
 			return nil
 		})
 	})
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(waitForEvents)
 	err := os.MkdirAll(filepath.Join(dir, "b"), 0755)
 	is.NoErr(err)
-	select {
-	case path := <-event:
-		is.Equal(path, filepath.Join(dir, "b"))
-	case <-time.After(5 * time.Second):
-		t.Fatal("timed out while waiting for watcher")
-	}
+	path, err := getEvent(event)
+	is.NoErr(err)
+	is.Equal(path, filepath.Join(dir, "b"))
 	err = os.WriteFile(filepath.Join(dir, "b", "a.txt"), []byte("b"), 0644)
 	is.NoErr(err)
+	path, err = getEvent(event)
+	is.NoErr(err)
+	is.Equal(path, filepath.Join(dir, "b", "a.txt"))
+	cancel()
+	is.NoErr(eg.Wait())
+}
+
+func TestWithScaffold(t *testing.T) {
+	is := is.New(t)
+	dir := t.TempDir()
+	ctx := context.Background()
+	event := make(chan string, 4)
+	ctx, cancel := context.WithCancel(context.Background())
+	eg := new(errgroup.Group)
+	eg.Go(func() error {
+		return watcher.Watch(ctx, dir, func(path string) error {
+			select {
+			case event <- path:
+			case <-ctx.Done():
+			}
+			return nil
+		})
+	})
+	time.Sleep(waitForEvents)
+	err := writeFiles(dir, map[string]string{
+		"controller/controller.go": `package controller`,
+		"view/index.svelte":        `<h1>index</h1>`,
+		"view/show.svelte":         `<h1>show</h1>`,
+	})
+	is.NoErr(err)
+	paths := map[string]bool{}
+	for i := 1; i <= 5; i++ {
+		path, err := getEvent(event)
+		is.NoErr(err)
+		rel, err := filepath.Rel(dir, path)
+		is.NoErr(err)
+		paths[rel] = true
+	}
+	is.True(paths["controller"])
+	is.True(paths["controller/controller.go"])
+	is.True(paths["view"])
+	is.True(paths["view/index.svelte"])
+	is.True(paths["view/show.svelte"])
+	// Test that there's no more events in the pipe
 	select {
 	case path := <-event:
-		is.Equal(path, filepath.Join(dir, "b", "a.txt"))
-	case <-time.After(5 * time.Second):
-		t.Fatal("timed out while waiting for watcher")
+		t.Fatalf("unexpected event: %q", path)
+	case <-time.Tick(waitForEvents):
 	}
 	cancel()
 	is.NoErr(eg.Wait())
