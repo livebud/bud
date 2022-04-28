165932dba23e48e307849b0cd75d98a6bb3763cc
Matthew Mueller <mattmuelle@gmail.com>
2022-04-28 01:51:09 -0500

    switch to node 16 to fix npm pack error. remove unused npm.Link code.

--- internal/npm/npm_test.go
+++ internal/npm/npm_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/livebud/bud/internal/npm"
-	"github.com/livebud/bud/package/vfs"
 	"github.com/matryer/is"
 )
 
@@ -30,24 +29,4 @@ func TestInstallSvelte(t *testing.T) {
 	exists(t, filepath.Join("_tmp", "node_modules", "svelte", "package.json"))
 	exists(t, filepath.Join("_tmp", "node_modules", "uid", "package.json"))
 	exists(t, filepath.Join("_tmp", "node_modules", "svelte", "internal", "index.js"))
-}
-func TestLinkLiveBud(t *testing.T) {
-	is := is.New(t)
-	is.NoErr(os.RemoveAll("_tmp"))
-	defer func() {
-		if !t.Failed() {
-			is.NoErr(os.RemoveAll("_tmp"))
-		}
-	}()
-	err := vfs.WriteAll(".", "_tmp", vfs.Memory{
-		"package.json": &vfs.File{Data: []byte(`{}`)},
-	})
-	is.NoErr(err)
-	err = npm.Link("../../livebud", "_tmp")
-	is.NoErr(err)
-	exists(t, filepath.Join("_tmp", "node_modules", "livebud", "package.json"))
-	exists(t, filepath.Join("_tmp", "node_modules", "livebud", "runtime", "svelte", "index.ts"))
-	exists(t, filepath.Join("_tmp", "node_modules", "livebud", "runtime", "hot", "index.ts"))
-	exists(t, filepath.Join("_tmp", "node_modules", "livebud", "runtime", "index.ts"))
-	exists(t, filepath.Join("_tmp", "node_modules", "livebud", "runtime", "jsx", "index.ts"))
 }
