7bb338ae97c2a992c90e23ab7d96cddcb8810988
codenoid <14269809+codenoid@users.noreply.github.com>
2022-05-14 17:27:08 +0700

    internal/gotemplate: add test

---
+++ internal/gotemplate/gotemplate_test.go
@@ -0,0 +1,37 @@
+package gotemplate_test
+
+import (
+	"testing"
+
+	"github.com/livebud/bud/internal/gotemplate"
+	"github.com/matryer/is"
+)
+
+func TestGenerateGoFile(t *testing.T) {
+	is := is.New(t)
+	template := `package main
+
+	func main()  {
+		  println("{{ .name }}")
+}`
+	expect := `package main
+
+func main() {
+	println("jason")
+}
+`
+	generator := gotemplate.MustParse("test.gotext", template)
+	b, err := generator.Generate(map[string]string{"name": "jason"})
+	is.NoErr(err)
+	is.Equal(string(b), expect)
+}
+
+func TestGenerateFreeText(t *testing.T) {
+	is := is.New(t)
+	template := `Hi {{ .name }}`
+	expect := `Hi Kim`
+	generator := gotemplate.MustParse("test.gotext", template)
+	b, err := generator.Generate(map[string]string{"name": "Kim"})
+	is.NoErr(err)
+	is.Equal(string(b), expect)
+}
