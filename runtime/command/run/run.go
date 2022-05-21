4afe2e06e5c30b817a664cd641802a884a61e145
012e <huyphmnhat@gmail.com>
2022-05-21 18:09:34 +0700

    Improved visual for hot reload

--- runtime/command/run/run.go
+++ runtime/command/run/run.go
@@ -2,8 +2,11 @@ package run
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"path/filepath"
+	"runtime"
+	"time"
 
 	"github.com/livebud/bud/package/exe"
 	"github.com/livebud/bud/package/log/console"
@@ -49,35 +52,66 @@ func (c *Command) compileAndStart(ctx context.Context, ln net.Listener) (*exe.Cm
 	return process, nil
 }
 
+func replacePreviousLine(msg string) {
+	// Move cursor up and delete line based on ANSI Escape Sequences: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+	switch runtime.GOOS {
+	case "windows":
+		// TODO: Test this on Windows, don't know will it work or not
+		fmt.Print("\033[1A\033[0K")
+	case "linux":
+		fmt.Print("\033[1A\033[0K")
+	default:
+		console.Error("Can't detect your operating system")
+	}
+	console.Info(msg)
+}
+
 func (c *Command) startApp(ctx context.Context, hotServer *hot.Server) error {
 	listener, err := socket.Load(c.Port)
 	if err != nil {
 		return err
 	}
+
+	// Init reload counter variable
+	totalReload := 0
+
 	// Compile and start the project
 	process, err := c.compileAndStart(ctx, listener)
 	if err != nil {
 		// TODO: de-duplicate with the watcher above
 		console.Error(err.Error())
 		if err := watcher.Watch(ctx, ".", func(path string) error {
+			totalReload++                       // Increase reload count
+			startTime := time.Now()             // Start timer
+			replacePreviousLine("Reloading...") // Handle reloading message
+
 			process, err = c.compileAndStart(ctx, listener)
 			if err != nil {
 				console.Error(err.Error())
+				console.Error("") // Avoid error messages getting overridden
+				totalReload = 0   // Reset reload counter
 				return nil
 			}
-			// TODO: host should be dynamic
-			console.Info("Ready on http://127.0.0.1" + c.Port)
+
+			// Time elapsed response
+			timeElapsed := time.Since(startTime).Milliseconds()
+			replacePreviousLine(fmt.Sprintf("Ready in %dms (x%d)", timeElapsed, totalReload))
 			return watcher.Stop
 		}); err != nil {
 			return err
 		}
 	}
 	defer process.Close()
+
 	// Start watching
 	if err := watcher.Watch(ctx, ".", func(path string) error {
 		switch filepath.Ext(path) {
 		// Re-compile the app and restart the Go server
 		case ".go":
+			totalReload++                       // Increase reload count
+			startTime := time.Now()             // Start timer
+			replacePreviousLine("Reloading...") // Handle reloading message
+
 			// Trigger a reload if there's a hot reload server configured
 			if hotServer != nil {
 				// Exclamation point just means full page reload
@@ -85,20 +119,28 @@ func (c *Command) startApp(ctx context.Context, hotServer *hot.Server) error {
 			}
 			if err := process.Close(); err != nil {
 				console.Error(err.Error())
+				console.Error("") // Avoid error messages getting overridden
+				totalReload = 0   // Reset reload counter
 				return nil
 			}
 			app, err := c.Project.Compile(ctx, c.Flag)
 			if err != nil {
 				console.Error(err.Error())
+				console.Error("") // Avoid error messages getting overridden
+				totalReload = 0   // Reset reload counter
 				return nil
 			}
 			process, err = app.Start(ctx, listener)
 			if err != nil {
 				console.Error(err.Error())
+				console.Error("") // Avoid error messages getting overridden
+				totalReload = 0   // Reset reload counter
 				return nil
 			}
-			// TODO: host should be dynamic
-			console.Info("Ready on http://127.0.0.1" + c.Port)
+
+			// Time elapsed response
+			timeElapsed := time.Since(startTime).Milliseconds()
+			replacePreviousLine(fmt.Sprintf("Ready in %dms (x%d)", timeElapsed, totalReload))
 			return nil
 		// Hot reload the page
 		default:
