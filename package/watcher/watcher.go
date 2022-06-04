86a9c4b8af7c3ad3f4a2c55673deb2e5b6c3fb0e
Matthew Mueller <mattmuelle@gmail.com>
2022-06-03 22:47:12 -0500

    package/watcher: dedupe events on linux and improve remove events

--- package/watcher/watcher.go
+++ package/watcher/watcher.go
@@ -6,6 +6,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	"golang.org/x/sync/errgroup"
 
@@ -23,6 +24,21 @@ func Watch(ctx context.Context, dir string, fn func(path string) error) error {
 		return err
 	}
 	defer watcher.Close()
+	// Avoid duplicate events
+	duplicates := map[string]struct{}{}
+	isDuplicate := func(path string, stat fs.FileInfo) bool {
+		stamp, err := computeStamp(path, stat)
+		if err != nil {
+			return false
+		}
+		// Duplicate check
+		if _, ok := duplicates[stamp]; ok {
+			return true
+		}
+		duplicates[stamp] = struct{}{}
+		return false
+	}
+	_ = isDuplicate
 	gitIgnore := gitignore.From(dir)
 	// Files to ignore while walking the directory
 	shouldIgnore := func(path string, de fs.DirEntry) error {
@@ -54,6 +70,14 @@ func Watch(ctx context.Context, dir string, fn func(path string) error) error {
 			return fn(path)
 		}
 	}
+	seen := map[string]struct{}{}
+	hasSeen := func(path string) bool {
+		if _, ok := seen[path]; ok {
+			return true
+		}
+		seen[path] = struct{}{}
+		return false
+	}
 	// For some reason renames are often emitted instead of
 	// Remove. Check it and correct.
 	rename := func(path string) error {
@@ -63,6 +87,10 @@ func Watch(ctx context.Context, dir string, fn func(path string) error) error {
 		}
 		// If it's a different error, ignore
 		if !errors.Is(err, fs.ErrNotExist) {
+			return nil
+		}
+		// Don't do anything if we've already seen this path
+		if hasSeen(path) {
 			return nil
 		}
 		// Remove the path and emit an update
@@ -76,6 +104,10 @@ func Watch(ctx context.Context, dir string, fn func(path string) error) error {
 	// Remove the file or directory from the watcher.
 	// We intentionally ignore errors for this case.
 	remove := func(path string) error {
+		// Don't do anything if we've already seen this path
+		if hasSeen(path) {
+			return nil
+		}
 		watcher.Remove(path)
 		// Trigger an update
 		if err := fn(path); err != nil {
@@ -88,11 +120,17 @@ func Watch(ctx context.Context, dir string, fn func(path string) error) error {
 	// If a new directory is created, add and trigger all the files within
 	// that directory.
 	create := func(path string) error {
+		// Reset seen path
+		delete(seen, path)
+		// Stat the file
 		stat, err := os.Stat(path)
 		if err != nil {
 			return nil
 		}
 		if gitIgnore(path, stat.IsDir()) {
+			return nil
+		}
+		if isDuplicate(path, stat) {
 			return nil
 		}
 		err = watcher.Add(path)
@@ -115,11 +153,17 @@ func Watch(ctx context.Context, dir string, fn func(path string) error) error {
 	}
 	// A file or directory has been updated. Notify our matchers.
 	write := func(path string) error {
-		fi, err := os.Stat(path)
+		// Reset seen path
+		delete(seen, path)
+		// Stat the file
+		stat, err := os.Stat(path)
 		if err != nil {
 			return nil
 		}
-		if gitIgnore(path, fi.IsDir()) {
+		if gitIgnore(path, stat.IsDir()) {
+			return nil
+		}
+		if isDuplicate(path, stat) {
 			return nil
 		}
 		// Trigger an update
@@ -178,4 +222,14 @@ func Watch(ctx context.Context, dir string, fn func(path string) error) error {
 		return nil
 	}
 	return nil
+}
+
+// computeStamp uses path, size, mode and modtime to try and ensure this is a
+// unique event.
+func computeStamp(path string, stat fs.FileInfo) (stamp string, err error) {
+	mtime := stat.ModTime().UnixNano()
+	mode := stat.Mode()
+	size := stat.Size()
+	stamp = path + ":" + strconv.Itoa(int(size)) + ":" + mode.String() + ":" + strconv.Itoa(int(mtime))
+	return stamp, nil
 }
--- package/watcher/watcher_test.go
+++ package/watcher/watcher_test.go
@@ -194,16 +194,10 @@ func TestWithScaffold(t *testing.T) {
 	is.True(paths["view"])
 	is.True(paths["view/index.svelte"])
 	is.True(paths["view/show.svelte"])
-	// While there should be no more events, testing this can be flaky in CI.
-	// Instead test that we don't have any events with unexpected paths.
-	// An extra event isn't the end of the world, it'll just reload one more time.
+	// Test that there's no more events in the pipe
 	select {
 	case path := <-event:
-		rel, err := filepath.Rel(dir, path)
-		is.NoErr(err)
-		if _, ok := paths[rel]; !ok {
-			t.Fatalf("unexpected event: %q", path)
-		}
+		t.Fatalf("unexpected event: %q", path)
 	case <-time.Tick(waitForEvents):
 	}
 	cancel()
