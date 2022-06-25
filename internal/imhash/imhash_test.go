c4e4f92f8267c17e640873941741be31bdb49ec6
Alex Suraci <suraci.alex@gmail.com>
2022-06-24 23:58:40 -0400

    support glob embeds

--- internal/imhash/imhash_test.go
+++ internal/imhash/imhash_test.go
@@ -62,3 +62,54 @@ func TestAppHash(t *testing.T) {
 	is.Equal(len(hash2), 11)
 	is.True(hash1 != hash2)
 }
+
+func TestAppHashEmbed(t *testing.T) {
+	is := is.New(t)
+	ctx := context.Background()
+	dir := t.TempDir()
+	td := testdir.New(dir)
+	td.Files["controller/controller.go"] = `
+		package controller
+		import "embed"
+		//go:embed *.sql
+		var Migrations embed.FS
+		type Controller struct {}
+		func (c *Controller) Index() string {
+			return "Hello Users!!"
+		}
+	`
+	td.Files["controller/1.up.sql"] = `CREATE UNIVERSE;`
+	td.Files["controller/1.down.sql"] = `DROP UNIVERSE;`
+	is.NoErr(td.Write(ctx))
+	cli := testcli.New(cli.New(dir))
+	stdout, stderr, err := cli.Run(ctx, "build")
+	is.NoErr(td.Exists(
+		"bud/.cli/main.go",
+		"bud/.app/main.go",
+	))
+	is.NoErr(err)
+	is.Equal(stdout.String(), "")
+	is.Equal(stderr.String(), "")
+	module, err := gomod.Find(dir)
+	is.NoErr(err)
+	hash1, err := imhash.Hash(module, "bud")
+	is.NoErr(err)
+	is.Equal(len(hash1), 11)
+	// Update
+	td.Files["controller/2.up.sql"] = `ALTER UNIVERSE;`
+	td.Files["controller/2.down.sql"] = `UNALTER UNIVERSE;`
+
+	is.NoErr(td.Write(ctx))
+	stdout, _, err = cli.Run(ctx, "build")
+	is.NoErr(err)
+	is.Equal(stdout.String(), "")
+	is.Equal(stderr.String(), "")
+	is.NoErr(td.Exists(
+		"bud/.cli/main.go",
+		"bud/.app/main.go",
+	))
+	hash2, err := imhash.Hash(module, "bud")
+	is.NoErr(err)
+	is.Equal(len(hash2), 11)
+	is.True(hash1 != hash2)
+}
