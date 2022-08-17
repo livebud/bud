42a2e2b75c54bb85f7e2133f3b1682f2c7d758ab
Matthew Mueller <mattmuelle@gmail.com>
2022-08-17 01:13:13 -0500

    add initial linking

--- internal/budfs/budfs.go
+++ internal/budfs/budfs.go
@@ -3,6 +3,7 @@ package budfs
 import (
 	"io/fs"
 	"net/http"
+	"path"
 
 	"github.com/livebud/bud/internal/cachefs"
 	"github.com/livebud/bud/internal/dag"
@@ -14,21 +15,26 @@ import (
 func New(fsys fs.FS, log log.Interface) *FileSystem {
 	gen := genfs.New()
 	cache := cachefs.New(log)
-	fsys = cache.Wrap(mergefs.New(fsys, gen))
+	merged := mergefs.New(gen, fsys)
+	syncfs := cache.Wrap(merged)
+	servefs := merged
 	return &FileSystem{
-		dag:  dag.New(),
-		gen:  gen,
-		fsys: fsys,
-		log:  log,
+		cache:   cache,
+		dag:     dag.New(),
+		gen:     gen,
+		log:     log,
+		syncfs:  syncfs,
+		servefs: servefs,
 	}
 }
 
 type FileSystem struct {
-	cache *cachefs.Cache
-	dag   *dag.Graph
-	gen   *genfs.FileSystem
-	fsys  fs.FS
-	log   log.Interface
+	cache   *cachefs.Cache
+	dag     *dag.Graph
+	gen     *genfs.FileSystem
+	log     log.Interface
+	syncfs  fs.FS
+	servefs fs.FS
 }
 
 type FS interface {
@@ -48,7 +54,7 @@ type DirGenerator interface {
 }
 
 func (f *FileSystem) Open(name string) (fs.File, error) {
-	file, err := f.fsys.Open(name)
+	file, err := f.syncfs.Open(name)
 	if err != nil {
 		return nil, err
 	}
@@ -62,26 +68,78 @@ type linkedFS struct {
 }
 
 func (f *linkedFS) Open(name string) (fs.File, error) {
+	f.dag.Link(f.path, name)
 	return f.fsys.Open(name)
 }
 
+// Link allows for arbitrary links
 func (f *linkedFS) Link(from, to string) {
-	// f.fsys.Link(from, to)
+	f.dag.Link(from, to)
+}
+
+type GenerateFile func(fsys FS, file *File) error
+
+func (fn GenerateFile) GenerateFile(fsys FS, file *File) error {
+	return fn(fsys, file)
 }
 
 func (f *FileSystem) FileGenerator(path string, generator FileGenerator) {
-	fsys := &linkedFS{f.dag, f.fsys, path}
+	fsys := &linkedFS{f.dag, f.syncfs, path}
 	f.gen.GenerateFile(path, func(file *genfs.File) error {
 		return generator.GenerateFile(fsys, file)
 	})
 }
 
+type GenerateDir func(fsys FS, dir *Dir) error
+
+func (fn GenerateDir) GenerateDir(fsys FS, dir *Dir) error {
+	return fn(fsys, dir)
+}
+
 func (f *FileSystem) DirGenerator(path string, generator DirGenerator) {
-	fsys := &linkedFS{f.dag, f.fsys, path}
+	fsys := &linkedFS{f.dag, f.syncfs, path}
 	f.gen.GenerateDir(path, func(dir *genfs.Dir) error {
 		return generator.GenerateDir(fsys, dir)
 	})
 }
 
 func (f *FileSystem) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (f *FileSystem) Print() string {
+	return f.dag.String()
+}
+
+// Create event
+func (f *FileSystem) Create(filepath string) {
+	dir := path.Dir(filepath)
+	f.cache.Delete(dir)
+	// Delete all downstream cache entries that depended on dir
+	for _, ancestor := range f.dag.Ancestors(dir) {
+		f.cache.Delete(ancestor)
+	}
+}
+
+// Update event
+func (f *FileSystem) Update(filepath string) {
+	f.cache.Delete(filepath)
+	// Delete all downstream cache entries that depended on filepath
+	for _, ancestor := range f.dag.Ancestors(filepath) {
+		f.cache.Delete(ancestor)
+	}
+}
+
+// Delete event
+func (f *FileSystem) Delete(filepath string) {
+	dir := path.Dir(filepath)
+	f.cache.Delete(filepath)
+	f.cache.Delete(dir)
+	// Delete all downstream cache entries that depended on filepath and dir
+	for _, ancestor := range f.dag.Ancestors(filepath) {
+		f.cache.Delete(ancestor)
+	}
+	for _, ancestor := range f.dag.Ancestors(dir) {
+		f.cache.Delete(ancestor)
+	}
 }
--- internal/budfs/budfs_test.go
+++ internal/budfs/budfs_test.go
@@ -1,6 +1,7 @@
 package budfs_test
 
 import (
+	"strconv"
 	"testing"
 	"testing/fstest"
 
@@ -13,7 +14,7 @@ import (
 	"github.com/livebud/bud/internal/is"
 )
 
-func TestReadRoot(t *testing.T) {
+func TestReadFsys(t *testing.T) {
 	is := is.New(t)
 	log := testlog.New()
 	fsys := fstest.MapFS{
@@ -23,4 +24,71 @@ func TestReadRoot(t *testing.T) {
 	des, err := fs.ReadDir(bfs, ".")
 	is.NoErr(err)
 	is.Equal(len(des), 1)
+}
+
+func TestGeneratorPriority(t *testing.T) {
+	is := is.New(t)
+	log := testlog.New()
+	fsys := fstest.MapFS{
+		"a.txt": &fstest.MapFile{Data: []byte("a")},
+	}
+	bfs := budfs.New(fsys, log)
+	bfs.FileGenerator("a.txt", budfs.GenerateFile(func(fsys budfs.FS, file *budfs.File) error {
+		file.Data = []byte("b")
+		return nil
+	}))
+	code, err := fs.ReadFile(bfs, "a.txt")
+	is.NoErr(err)
+	is.Equal(string(code), "b")
+}
+
+func TestCaching(t *testing.T) {
+	is := is.New(t)
+	log := testlog.New()
+	fsys := fstest.MapFS{}
+	bfs := budfs.New(fsys, log)
+	count := 1
+	bfs.FileGenerator("a.txt", budfs.GenerateFile(func(fsys budfs.FS, file *budfs.File) error {
+		file.Data = []byte(strconv.Itoa(count))
+		count++
+		return nil
+	}))
+	code, err := fs.ReadFile(bfs, "a.txt")
+	is.NoErr(err)
+	is.Equal(string(code), "1")
+	code, err = fs.ReadFile(bfs, "a.txt")
+	is.NoErr(err)
+	is.Equal(string(code), "1")
+}
+
+func TestLinking(t *testing.T) {
+	is := is.New(t)
+	log := testlog.New()
+	fsys := fstest.MapFS{}
+	bfs := budfs.New(fsys, log)
+	count := 1
+	bfs.FileGenerator("a.txt", budfs.GenerateFile(func(fsys budfs.FS, file *budfs.File) error {
+		file.Data = []byte(strconv.Itoa(count))
+		count++
+		return nil
+	}))
+	bfs.FileGenerator("b.txt", budfs.GenerateFile(func(fsys budfs.FS, file *budfs.File) error {
+		code, err := fs.ReadFile(fsys, "a.txt")
+		if err != nil {
+			return err
+		}
+		file.Data = []byte(code)
+		return nil
+	}))
+	code, err := fs.ReadFile(bfs, "b.txt")
+	is.NoErr(err)
+	is.Equal(string(code), "1")
+	// Cached
+	code, err = fs.ReadFile(bfs, "b.txt")
+	is.NoErr(err)
+	is.Equal(string(code), "1")
+	bfs.Update("a.txt")
+	code, err = fs.ReadFile(bfs, "b.txt")
+	is.NoErr(err)
+	is.Equal(string(code), "2")
 }
