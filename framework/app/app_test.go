083185c7af0b8ff90ee31e4c5a306988ead4928e
EC2 Default User <ec2-user@ip-172-31-40-12.us-east-2.compute.internal>
2022-09-02 02:34:56 +0000

    broken test

--- framework/app/app_test.go
+++ framework/app/app_test.go
@@ -2,11 +2,13 @@ package app_test
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/livebud/bud/internal/cli/testcli"
 	"github.com/livebud/bud/internal/is"
 	"github.com/livebud/bud/internal/testdir"
+	"github.com/livebud/bud/package/socket"
 )
 
 func TestWelcome(t *testing.T) {
@@ -29,4 +31,26 @@ func TestWelcome(t *testing.T) {
 	is.Equal(app.Stderr(), "")
 	is.NoErr(td.Exists("bud/app"))
 	is.NoErr(app.Close())
+}
+
+func TestOneUpPort(t *testing.T) {
+	is := is.New(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	_, err := socket.Listen(":3000")
+	is.NoErr(err)
+	dir := t.TempDir()
+	td := testdir.New(dir)
+	is.NoErr(td.Write(ctx))
+	cli := testcli.New(dir)
+	is.NoErr(td.NotExists("bud/app"))
+	app, err := cli.Start(ctx, "run")
+	is.NoErr(err)
+	_, err = app.Get("/")
+	is.NoErr(err)
+	req, err := http.NewRequest("GET", "http://localhost:3000/", nil)
+	is.NoErr(err)
+	res, err := http.DefaultClient.Do(req)
+	is.NoErr(err)
+	defer res.Body.Close()
 }
