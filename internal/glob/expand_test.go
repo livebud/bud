bd7a1a08a4a6da7a443edda27b7b26eb2e17fa07
Matthew Mueller <mattmuelle@gmail.com>
2022-09-05 03:33:32 -0500

    add commented out failing test

--- internal/glob/expand_test.go
+++ internal/glob/expand_test.go
@@ -64,4 +64,5 @@ func TestExpand(t *testing.T) {
 	test("{controller/**.go,view/**}", "controller/**.go", "view/**")
 	test("{{controller,view}/**.go,view/**}", "controller/**.go", "view/**.go", "view/**")
 	test("{controller,controller}", "controller")
+	// TODO support: test("{a,b}/{c,d}", "a/c", "a/d", "b/c", "b/d")
 }
