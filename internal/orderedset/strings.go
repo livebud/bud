c12897b5cfacdf6ba7c28ea80fdb040e92134596
Matthew Mueller <mattmuelle@gmail.com>
2022-09-13 02:02:22 -0500

    remove generics

--- internal/orderedset/strings.go
+++ internal/orderedset/strings.go
@@ -12,38 +12,3 @@ func Strings(list ...string) []string {
 	}
 	return set
 }
-
-func New[T comparable]() *Set[T] {
-	return &Set[T]{
-		seen: map[T]struct{}{},
-		set:  []T{},
-	}
-}
-
-type Set[T comparable] struct {
-	seen map[T]struct{}
-	set  []T
-}
-
-func (s *Set[T]) Add(items ...T) {
-	for _, item := range items {
-		s.add(item)
-	}
-}
-
-func (s *Set[T]) add(item T) {
-	if _, ok := s.seen[item]; ok {
-		return
-	}
-	s.seen[item] = struct{}{}
-	s.set = append(s.set, item)
-}
-
-func (s *Set[T]) Has(item T) bool {
-	_, ok := s.seen[item]
-	return ok
-}
-
-func (s *Set[T]) List() []T {
-	return s.set
-}
--- internal/orderedset/strings_test.go
+++ internal/orderedset/strings_test.go
@@ -19,18 +19,3 @@ func TestOrderedStringSet(t *testing.T) {
 	)
 	is.Equal(orderedset.Strings(), []string{})
 }
-
-func TestNewSet(t *testing.T) {
-	is := is.New(t)
-	// Empty
-	s := orderedset.New[string]()
-	is.Equal(s.List(), []string{})
-	// With duplicates
-	s = orderedset.New[string]()
-	s.Add("a", "a", "a")
-	is.Equal(s.List(), []string{"a"})
-	// With others
-	s = orderedset.New[string]()
-	s.Add("a", "c", "b", "b", "d", "a", "c", "d", "e")
-	is.Equal(s.List(), []string{"a", "c", "b", "d", "e"})
-}
