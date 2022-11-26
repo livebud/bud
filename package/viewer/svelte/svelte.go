33cff5c2dbc517bf7495596f48db44443dc11165
Matthew Mueller <mattmuelle@gmail.com>
2022-11-25 21:16:42 -0600

    package/viewer/svelte: get client-side working with frames

--- package/viewer/svelte/svelte.go
+++ package/viewer/svelte/svelte.go
@@ -37,7 +37,7 @@ type Viewer struct {
 var _ viewer.Viewer = (*Viewer)(nil)
 
 func (v *Viewer) Render(ctx context.Context, page *viewer.Page) ([]byte, error) {
-	code, err := v.compile(page)
+	code, err := v.compileSSR(page)
 	if err != nil {
 		return nil, err
 	}
@@ -61,15 +61,12 @@ func (v *Viewer) RenderError(ctx context.Context, page *viewer.Page) []byte {
 	return code
 }
 
-func (v *Viewer) RegisterPage(r *router.Router, page *viewer.Page) {
-}
-
-func (v *Viewer) compile(page *viewer.Page) (code []byte, err error) {
+func (v *Viewer) compileSSR(page *viewer.Page) (code []byte, err error) {
 	result := esbuild.Build(esbuild.BuildOptions{
 		EntryPointsAdvanced: []esbuild.EntryPoint{
 			{
 				InputPath:  page.Path + ".js",
-				OutputPath: path.Join(page.Path + ".js"),
+				OutputPath: page.Path + ".js",
 			},
 		},
 		AbsWorkingDir: v.module.Directory(),
@@ -80,9 +77,9 @@ func (v *Viewer) compile(page *viewer.Page) (code []byte, err error) {
 		Bundle:        true,
 		// Metafile:      true,
 		Plugins: []esbuild.Plugin{
-			v.pagePlugin(page),
-			v.svelteRuntime(),
-			v.sveltePlugin(),
+			v.ssrSveltePage(page),
+			v.ssrSvelteRuntime(),
+			v.ssrSvelteTransform(),
 			v.httpPlugin(),
 			v.esmPlugin(),
 		},
@@ -103,15 +100,15 @@ func (v *Viewer) compile(page *viewer.Page) (code []byte, err error) {
 	return ssrCode, nil
 }
 
-//go:embed page.gotext
-var pageCode string
+//go:embed ssr.gotext
+var ssrCode string
 
-var pageTemplate = gotemplate.MustParse("svelte/page.gotext", pageCode)
+var ssrTemplate = gotemplate.MustParse("ssr.gotext", ssrCode)
 
 // page plugin creates a virtual page point for the page
-func (v *Viewer) pagePlugin(page *viewer.Page) esbuild.Plugin {
+func (v *Viewer) ssrSveltePage(page *viewer.Page) esbuild.Plugin {
 	return esbuild.Plugin{
-		Name: "bud-svelte-page",
+		Name: "ssrSveltePage",
 		Setup: func(epb esbuild.PluginBuild) {
 			epb.OnResolve(esbuild.OnResolveOptions{Filter: `^` + page.Path + `\.js$`}, func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
 				result.Namespace = page.Path + `.js`
@@ -119,7 +116,7 @@ func (v *Viewer) pagePlugin(page *viewer.Page) esbuild.Plugin {
 				return result, nil
 			})
 			epb.OnLoad(esbuild.OnLoadOptions{Filter: `.*`, Namespace: page.Path + `.js`}, func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
-				code, err := pageTemplate.Generate(page)
+				code, err := ssrTemplate.Generate(page)
 				if err != nil {
 					return result, err
 				}
@@ -133,21 +130,21 @@ func (v *Viewer) pagePlugin(page *viewer.Page) esbuild.Plugin {
 	}
 }
 
-//go:embed runtime.ts
-var runtimeCode string
+//go:embed ssr.ts
+var ssrRuntime string
 
-func (v *Viewer) svelteRuntime() esbuild.Plugin {
+func (v *Viewer) ssrSvelteRuntime() esbuild.Plugin {
 	return esbuild.Plugin{
-		Name: "bud-svelte-runtime",
+		Name: "ssrSvelteRuntime",
 		Setup: func(epb esbuild.PluginBuild) {
-			epb.OnResolve(esbuild.OnResolveOptions{Filter: `^bud-svelte-runtime$`}, func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
-				result.Namespace = "svelte-runtime"
+			epb.OnResolve(esbuild.OnResolveOptions{Filter: `^ssrSvelteRuntime$`}, func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
+				result.Namespace = "ssrSvelteRuntime"
 				result.Path = args.Path
 				return result, nil
 			})
-			epb.OnLoad(esbuild.OnLoadOptions{Filter: `.*`, Namespace: `svelte-runtime`}, func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
+			epb.OnLoad(esbuild.OnLoadOptions{Filter: `.*`, Namespace: `ssrSvelteRuntime`}, func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
 				result.ResolveDir = v.module.Directory()
-				result.Contents = &runtimeCode
+				result.Contents = &ssrRuntime
 				result.Loader = esbuild.LoaderTS
 				return result, nil
 			})
@@ -156,9 +153,9 @@ func (v *Viewer) svelteRuntime() esbuild.Plugin {
 }
 
 // Svelte plugin transforms Svelte imports to server-side JS
-func (v *Viewer) sveltePlugin() esbuild.Plugin {
+func (v *Viewer) ssrSvelteTransform() esbuild.Plugin {
 	return esbuild.Plugin{
-		Name: "bud-svelte",
+		Name: "ssrSvelteTransform",
 		Setup: func(epb esbuild.PluginBuild) {
 			epb.OnResolve(esbuild.OnResolveOptions{Filter: `\.svelte$`}, func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
 				result.Namespace = "svelte"
@@ -167,6 +164,137 @@ func (v *Viewer) sveltePlugin() esbuild.Plugin {
 			})
 			epb.OnLoad(esbuild.OnLoadOptions{Filter: `.*`, Namespace: `svelte`}, func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
 				code, err := v.transformer.Transform(v.fsys, path.Join("ssr.js", args.Path))
+				if err != nil {
+					return result, err
+				}
+				contents := string(code)
+				result.ResolveDir = v.module.Directory()
+				result.Contents = &contents
+				result.Loader = esbuild.LoaderJS
+				return result, nil
+			})
+		},
+	}
+}
+
+func (v *Viewer) RegisterClient(r *router.Router, page *viewer.Page) {
+	r.Get(page.Client, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		code, err := v.compileDOM(page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/javascript")
+		w.Write(code)
+	}))
+}
+
+func (v *Viewer) compileDOM(page *viewer.Page) (code []byte, err error) {
+	result := esbuild.Build(esbuild.BuildOptions{
+		EntryPointsAdvanced: []esbuild.EntryPoint{
+			{
+				InputPath:  page.Client,
+				OutputPath: page.Client,
+			},
+		},
+		AbsWorkingDir:     v.module.Directory(),
+		Outdir:            "./",
+		Format:            esbuild.FormatIIFE,
+		Platform:          esbuild.PlatformBrowser,
+		GlobalName:        "bud",
+		MinifyIdentifiers: false,
+		MinifySyntax:      false,
+		MinifyWhitespace:  false,
+		Bundle:            true,
+		// Metafile:      true,
+		Plugins: []esbuild.Plugin{
+			v.domSveltePage(page),
+			v.domSvelteRuntime(),
+			v.domSvelteTransform(),
+			v.httpPlugin(),
+			v.esmPlugin(),
+		},
+	})
+	if len(result.Errors) > 0 {
+		msgs := esbuild.FormatMessages(result.Errors, esbuild.FormatMessagesOptions{
+			Color:         true,
+			Kind:          esbuild.ErrorMessage,
+			TerminalWidth: 80,
+		})
+		return nil, fmt.Errorf(strings.Join(msgs, "\n"))
+	}
+	// Expect exactly 1 output file
+	if len(result.OutputFiles) != 1 {
+		return nil, fmt.Errorf("expected exactly 1 output file but got %d", len(result.OutputFiles))
+	}
+	ssrCode := result.OutputFiles[0].Contents
+	return ssrCode, nil
+}
+
+//go:embed dom.gotext
+var domCode string
+
+var domTemplate = gotemplate.MustParse("dom.gotext", domCode)
+
+// page plugin creates a virtual page point for the page
+func (v *Viewer) domSveltePage(page *viewer.Page) esbuild.Plugin {
+	return esbuild.Plugin{
+		Name: "domSveltePage",
+		Setup: func(epb esbuild.PluginBuild) {
+			epb.OnResolve(esbuild.OnResolveOptions{Filter: `^` + page.Client + `$`}, func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
+				result.Namespace = "domSveltePage"
+				result.Path = args.Path
+				return result, nil
+			})
+			epb.OnLoad(esbuild.OnLoadOptions{Filter: `^` + page.Client + `$`, Namespace: "domSveltePage"}, func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
+				code, err := domTemplate.Generate(page)
+				if err != nil {
+					return result, err
+				}
+				contents := string(code)
+				result.ResolveDir = v.module.Directory()
+				result.Contents = &contents
+				result.Loader = esbuild.LoaderJS
+				return result, nil
+			})
+		},
+	}
+}
+
+//go:embed dom.ts
+var domRuntime string
+
+func (v *Viewer) domSvelteRuntime() esbuild.Plugin {
+	return esbuild.Plugin{
+		Name: "domSvelteRuntime",
+		Setup: func(epb esbuild.PluginBuild) {
+			epb.OnResolve(esbuild.OnResolveOptions{Filter: `^domSvelteRuntime$`}, func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
+				result.Namespace = "domSvelteRuntime"
+				result.Path = args.Path
+				return result, nil
+			})
+			epb.OnLoad(esbuild.OnLoadOptions{Filter: `.*`, Namespace: `domSvelteRuntime`}, func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
+				result.ResolveDir = v.module.Directory()
+				result.Contents = &domRuntime
+				result.Loader = esbuild.LoaderTS
+				return result, nil
+			})
+		},
+	}
+}
+
+// Svelte plugin transforms Svelte imports to server-side JS
+func (v *Viewer) domSvelteTransform() esbuild.Plugin {
+	return esbuild.Plugin{
+		Name: "domSvelteTransform",
+		Setup: func(epb esbuild.PluginBuild) {
+			epb.OnResolve(esbuild.OnResolveOptions{Filter: `\.svelte$`}, func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
+				result.Namespace = "svelte"
+				result.Path = args.Path
+				return result, nil
+			})
+			epb.OnLoad(esbuild.OnLoadOptions{Filter: `.*`, Namespace: `svelte`}, func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
+				code, err := v.transformer.Transform(v.fsys, path.Join("js", args.Path))
 				if err != nil {
 					return result, err
 				}
--- package/viewer/viewer.go
+++ package/viewer/viewer.go
@@ -2,37 +2,29 @@ package viewer
 
 import (
 	"context"
-	"path"
 
 	"github.com/livebud/bud/package/router"
 )
 
-type Transpiler interface {
-	Transpile(name string, src []byte) ([]byte, error)
-}
-
 type Viewer interface {
 	Render(ctx context.Context, page *Page) ([]byte, error)
 	RenderError(ctx context.Context, page *Page) []byte
-	RegisterPage(r *router.Router, page *Page)
+	RegisterClient(r *router.Router, page *Page)
 }
 
 type Page struct {
 	*View
 	Layout *View   `json:"layout,omitempty"`
 	Frames []*View `json:"frames,omitempty"`
-}
-
-func (p *Page) Client() string {
-	return path.Join("bud", p.Path+".js")
+	Client string  `json:"client,omitempty"`
 }
 
 type Props = map[string]interface{}
 type Context = map[string]interface{}
 
 type View struct {
-	Key     string  `json:"key,omitempty"`
-	Path    string  `json:"path,omitempty"` // Can be empty
-	Props   Props   `json:"props,omitempty"`
-	Context Context `json:"context,omitempty"`
+	Key     string  `json:"key,omitempty"`     // Can be blank
+	Path    string  `json:"path,omitempty"`    // Can be blank
+	Props   Props   `json:"props,omitempty"`   // Can be nil
+	Context Context `json:"context,omitempty"` // Can be nil
 }
