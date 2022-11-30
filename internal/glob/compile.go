d2dedf10f6795e530c279c3bd85b06b2b222bac6
Matthew Mueller <mattmuelle@gmail.com>
2022-11-29 23:39:01 -0600

    internal/glob: fix wildcards in multi-select causing infinite recursion in the generators.

--- internal/glob/compile.go
+++ internal/glob/compile.go
@@ -2,7 +2,35 @@ package glob
 
 import "github.com/gobwas/glob"
 
-// Alias for glob.Glob
-var Compile = glob.Compile
-
 type Matcher = glob.Glob
+
+// Comple
+func Compile(pattern string) (Matcher, error) {
+	// Expand patterns like {a,b} into multiple globs a & b. This avoids an
+	// infinite loop described in this comment:
+	// https://github.com/gobwas/glob/issues/50#issuecomment-1330182417
+	patterns, err := Expand(pattern)
+	if err != nil {
+		return nil, err
+	}
+	globs := make(globs, len(patterns))
+	for i, pattern := range patterns {
+		glob, err := glob.Compile(pattern)
+		if err != nil {
+			return nil, err
+		}
+		globs[i] = glob
+	}
+	return globs, nil
+}
+
+type globs []glob.Glob
+
+func (globs globs) Match(path string) bool {
+	for _, glob := range globs {
+		if glob.Match(path) {
+			return true
+		}
+	}
+	return false
+}
--- internal/glob/compile_test.go
+++ internal/glob/compile_test.go
@@ -3,7 +3,7 @@ package glob_test
 import (
 	"testing"
 
-	"github.com/gobwas/glob"
+	"github.com/livebud/bud/internal/glob"
 	"github.com/livebud/bud/internal/is"
 )
 
@@ -19,7 +19,15 @@ func TestDirMatch(t *testing.T) {
 	is := is.New(t)
 	matcher, err := glob.Compile("controller/*/**.go")
 	is.NoErr(err)
+	is.True(!matcher.Match("controller"))
 	is.True(!matcher.Match("controller/controller.go"))
 	is.True(matcher.Match("controller/view/view.go"))
 	is.True(matcher.Match("controller/public/public.go"))
+}
+
+func TestMatchSubdir(t *testing.T) {
+	is := is.New(t)
+	matcher, err := glob.Compile(`{generator/**.go,bud/internal/generator/*/**.go}`)
+	is.NoErr(err)
+	is.True(!matcher.Match("bud/internal/generator/generator.go"))
 }
