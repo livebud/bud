f20ed70275e22a8946807797c8433389bbc2ea35
Matt Mueller <mattmuelle@gmail.com>
2022-12-13 02:05:29 -0600

    once/closer: refactor api (#338)

--- internal/once/closer_test.go
+++ internal/once/closer_test.go
@@ -13,22 +13,10 @@ func TestCloserOk(t *testing.T) {
 	var closer once.Closer
 	a := func() error { return nil }
 	b := func() error { return nil }
-	closer.Closes = append(closer.Closes, a)
-	closer.Closes = append(closer.Closes, b)
+	closer.Add(a)
+	closer.Add(b)
 	err := closer.Close()
 	is.NoErr(err)
-}
-
-func TestCloserReason(t *testing.T) {
-	is := is.New(t)
-	e := errors.New("error")
-	var closer once.Closer
-	a := func() error { return nil }
-	b := func() error { return nil }
-	closer.Closes = append(closer.Closes, a)
-	closer.Closes = append(closer.Closes, b)
-	err := closer.Close(e)
-	is.True(errors.Is(err, e))
 }
 
 func TestCloserErrors(t *testing.T) {
@@ -38,8 +26,8 @@ func TestCloserErrors(t *testing.T) {
 	var closer once.Closer
 	a := func() error { return e1 }
 	b := func() error { return e2 }
-	closer.Closes = append(closer.Closes, a)
-	closer.Closes = append(closer.Closes, b)
+	closer.Add(a)
+	closer.Add(b)
 	err := closer.Close()
 	is.True(err != nil)
 	is.Equal(err.Error(), "error 2. error 1")
