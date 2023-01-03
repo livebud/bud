90a42e08a92f0c9acda4bddbd426fca7ffcbd15a
Matthew Mueller <mattmuelle@gmail.com>
2023-01-03 00:39:55 -0600

    install.sh: remove GITHUB_TOKEN usage in install.sh. Fixes: #348

--- install.sh
+++ install.sh
@@ -236,11 +236,6 @@ github_api() {
   local_file=$1
   source_url=$2
   header=""
-  case "$source_url" in
-    https://api.github.com*)
-      test -z "$GITHUB_TOKEN" || header="Authorization: token $GITHUB_TOKEN"
-      ;;
-  esac
   http_download "$local_file" "$source_url" "$header"
 }
 github_last_release() {
