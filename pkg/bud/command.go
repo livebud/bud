e40a311142bd3448ae63ae9ced37e70e87bcb401
Matthew Mueller <mattmuelle@gmail.com>
2023-09-09 15:45:14 -0500

    add routes command

--- pkg/bud/command.go
+++ pkg/bud/command.go
@@ -8,11 +8,12 @@ import (
 
 	"github.com/livebud/bud/pkg/cli"
 	"github.com/livebud/bud/pkg/env"
+	"github.com/livebud/bud/pkg/mux"
 	"github.com/livebud/bud/pkg/web"
 )
 
-func New(env *env.Bud, log *slog.Logger, server *web.Server) *Command {
-	return &Command{env, log, server}
+func New(env *env.Bud, log *slog.Logger, router *mux.Router, server *web.Server) *Command {
+	return &Command{env, log, router, server}
 }
 
 func Register(cli cli.Command, cmd *Command) {
@@ -21,11 +22,14 @@ func Register(cli cli.Command, cmd *Command) {
 	cli.Run(func(ctx context.Context) error {
 		return cmd.Run(ctx, run)
 	})
+	routes := cli.Command("routes", "list the router routes")
+	routes.Run(cmd.Routes)
 }
 
 type Command struct {
 	env    *env.Bud
 	log    *slog.Logger
+	router *mux.Router
 	server *web.Server
 }
 
@@ -41,4 +45,11 @@ func (c *Command) Run(ctx context.Context, in *Run) error {
 	}
 	c.log.Info(fmt.Sprintf("Listening on %s", web.Format(ln)))
 	return c.server.Serve(ctx, ln)
+}
+
+func (c *Command) Routes(ctx context.Context) error {
+	for _, route := range c.router.List() {
+		fmt.Println(route.String())
+	}
+	return nil
 }
